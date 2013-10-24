fb384872fd094810f70fd5c6347d5e1afc1873a9
Beta Ziliani <beta@mpi-sws.org>
2013-10-24 16:11:48 +0200

    Adding M inductive type

---
+++ plugins/mtac2/theories/Mtac2.v
@@ -0,0 +1,147 @@
+
+
+Inductive Exception : Type := exception : Exception.
+
+
+Inductive Mtac2 : Type -> Prop :=
+| Mret : forall {A}, A -> Mtac2 A
+| Mbind : forall {A B}, Mtac2 A -> (A -> Mtac2 B) -> Mtac2 B
+| Mtry : forall {A}, Mtac2 A -> (Exception -> Mtac2 A) -> Mtac2 A
+| Mraise : forall {A}, Exception -> Mtac2 A
+| Mfix1' : forall {A B} (S : Type -> Prop), 
+  (forall a, S a -> Mtac2 a) ->
+  ((forall x : A, S (B x)) -> (forall x : A, S (B x))) -> 
+  forall x : A, Mtac2 (B x)
+| Mfix2' : forall {A1 A2 B} (S : Type -> Prop), 
+  (forall a, S a -> Mtac2 a) ->
+  ((forall (x1 : A1) (x2 : A2 x1), S (B x1 x2)) -> 
+    (forall (x1 : A1) (x2 : A2 x1), S (B x1 x2))) -> 
+  forall (x1 : A1) (x2 : A2 x1), Mtac2 (B x1 x2)
+| Mfix3' : forall {A1 A2 A3 B} (S : Type -> Prop), 
+  (forall a, S a -> Mtac2 a) ->
+  ((forall (x1 : A1) (x2 : A2 x1) (x3 : A3 x1 x2), S (B x1 x2 x3)) -> 
+    (forall (x1 : A1) (x2 : A2 x1) (x3 : A3 x1 x2), S (B x1 x2 x3))) -> 
+  forall (x1 : A1) (x2 : A2 x1) (x3 : A3 x1 x2), Mtac2 (B x1 x2 x3)
+| Mmatch : forall {A} B (t : A), list (Mpatt A B t) -> Mtac2 (B t)
+
+with Mpatt : forall A (B : A -> Type) (t : A), Type := 
+| Mbase : forall {A B t} (x:A) (b : t = x -> Mtac2 (B x)), Mpatt A B t
+| Mtele : forall {A B C t}, (forall (x : C), Mpatt A B t) -> Mpatt A B t.
+
+
+Definition Mfix1 {A} B := @Mfix1' A B Mtac2 (fun _ x => x).
+Definition Mfix2 {A1 A2} B := @Mfix2' A1 A2 B Mtac2 (fun _ x => x).
+Definition Mfix3 {A1 A2 A3} B := @Mfix3' A1 A2 A3 B Mtac2 (fun _ x => x).
+
+
+Module Mtac2Notations.
+
+Notation "'M'" := Mtac2.
+
+Notation "'ret'" := Mret.
+Notation "'raise'" := Mraise.
+
+Notation "r '<-' t1 ';' t2" := (@Mbind _ _ t1 (fun r=> t2)) 
+  (at level 80, right associativity). 
+Notation "t1 ';;' t2" := (@Mbind _ _ t1 (fun _=>t2)) 
+  (at level 81, right associativity).
+Notation "f @@ x" := (Mbind f (fun r=>ret (r x))) (at level 70).
+Notation "f >> x" := (Mbind f (fun r=>x r)) (at level 70).
+
+Notation "[ x .. y ] ps" := (Mtele (fun x=> .. (Mtele (fun y=>ps)).. ))
+  (at level 202, x binder, y binder, ps at next level) : mtac_patt_scope.
+Notation "p => b" := (Mbase p%core (fun _=>b%core)) 
+  (no associativity, at level 201) : mtac_patt_scope. 
+Notation "p => [ H ] b" := (Mbase p%core (fun H=>b%core)) 
+  (no associativity, at level 201, H at next level) : mtac_patt_scope. 
+Notation "'_' => b " := (Mtele (fun x=> Mbase x (fun _=>b%core))) 
+  (at level 201, b at next level) : mtac_patt_scope.
+
+Delimit Scope mtac_patt_scope with mtac_patt.
+
+Notation "'mmatch' t 'with' | p1 | .. | pn 'end'" := 
+  (Mmatch (fun _=>_) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, only parsing).
+Notation "'mmatch' t 'return' 'M' p 'with' | p1 | .. | pn 'end'" := 
+  (Mmatch (fun _=>p) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, only parsing).
+Notation "'mmatch' t 'as' x 'return' 'M' p 'with' | p1 | .. | pn 'end'" := 
+  (Mmatch (fun x=>p) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, format
+  "'[v' 'mmatch'  t  'as'  x  'return'  'M'  p  'with' '/' '|'  p1 '/' '|'  .. '/' '|'  pn '/' 'end' ']'").
+
+Notation "'mmatch' t 'with' p1 | .. | pn 'end'" := 
+  (Mmatch (fun _=>_) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, only parsing).
+Notation "'mmatch' t 'return' 'M' p 'with' p1 | .. | pn 'end'" := 
+  (Mmatch (fun _=>p) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, only parsing).
+Notation "'mmatch' t 'as' x 'return' 'M' p 'with' p1 | .. | pn 'end'" := 
+  (Mmatch (fun x=>p) t (cons p1%mtac_patt (.. (cons pn%mtac_patt nil) ..))) 
+    (at level 90, p1 at level 210, pn at level 210, only parsing).
+
+
+Notation "'mfix1' f ( x : A ) : 'M' B := b" := (@Mfix1 A (fun x=>B) (fun f (x : A)=>b))
+  (at level 85, f at level 0, x at next level, format
+  "'[v  ' 'mfix1'  f  '(' x  ':'  A ')'  ':'  'M'  B  ':=' '/  ' b ']'").
+
+Notation "'mfix2' f ( x : A1 ) ( y : A2 ) : 'M' B := b" := 
+  (@Mfix2 A1 (forall x, A2) (fun x y=>B) (fun f (x : A1) (y : A2)=>b))
+  (at level 85, f at level 0, x at next level, y at next level, format
+  "'[v  ' 'mfix2'  f  '(' x  ':'  A1 ')'  '(' y  ':'  A2 ')'  ':'  'M'  B  ':=' '/  ' b ']'").
+
+Notation "'mfix3' f ( x : A1 ) ( y : A2 ) ( z : A3 ) : 'M' B := b" := 
+  (@Mfix3 A1 (forall x, A2) (forall x y, A3) (fun x y z=>B) 
+    (fun f (x : A1) (y : A2) (z : A3)=>b))
+  (at level 85, f at level 0, x at next level, y at next level, z at next level, format
+  "'[v  ' 'mfix3'  f  '(' x  ':'  A1 ')'  '(' y  ':'  A2 ')'  '(' z  ':'  A3 ')'  ':'  'M'  B  ':=' '/  ' b ']'").
+
+
+Notation "'mtry' a 'with' | p1 | .. | pn 'end'" := 
+  (Mtry a (fun e=>
+    (Mmatch (fun _=>_) e (cons p1%mtac_patt (.. (cons pn%mtac_patt (cons (Mbase e (fun _ =>Mraise e)) nil)) ..)))))
+    (at level 82, p1 at level 210, pn at level 210, only parsing).
+
+Notation "'mtry' a 'with' p1 | .. | pn 'end'" := 
+  (Mtry a (fun e=>
+    (Mmatch (fun _=>_) e (cons p1%mtac_patt (.. (cons pn%mtac_patt (cons (Mbase e (fun _ =>Mraise e)) nil)) ..)))))
+    (at level 82, p1 at level 210, pn at level 210, only parsing).
+
+Notation "'mtry' a 'as' e 'in' | p1 | .. | pn 'end'" := 
+  (Mtry a (fun e=>Mmatch (fun _=>_) e (cons p1%mtac_patt (.. (cons pn%mtac_patt (cons (Mbase e (fun _=>raise e)) nil)) ..))))
+    (at level 82, e at next level, p1 at level 210, pn at level 210, format 
+   "'[v' 'mtry' '/  '  a  '/' 'as'  e  'in' '/' '|'  p1  '/' '|'  ..  '/' '|'  pn '/' 'end' ']'"
+).
+
+
+Require Import List.
+
+Program
+Definition example {A} (x : A) :=
+  mfix1 f (s : list A) : M (In x s) :=
+    mmatch s with
+    | [l r] l ++ r =>
+      mtry 
+        p <- f l;
+        ret _
+      with
+      | exception => 
+        p <- f r;
+        ret _
+      end
+    | [s'] x :: s' => ret (in_eq _ _)
+    | [y s'] y :: s' => 
+      r <- f s';
+      ret (in_cons _ _ _ r)
+    | _ => raise exception
+    end. 
+Next Obligation.
+intros. fold (In x (l++r)). apply in_or_app.
+left; assumption.
+Qed.
+Next Obligation.
+intros. fold (In x (l++r)). apply in_or_app.
+right; assumption.
+Qed.
+
+Print example.
